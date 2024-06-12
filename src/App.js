2e27be352026ed0cb91e7b420777481b64cefe7b
Mudassir03 <118797440+Mudassir03@users.noreply.github.com>
2024-06-13 00:29:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Search from "./Search";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/*Search bar */}
+      <Search defaultData="HYDERABAD" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Credits.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Credits.css";
+
+export default function Credits() {
+  return (
+    <div className="Credits">
+      <div className="container">
+        <div className="row">
+          <div className="col-12"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
